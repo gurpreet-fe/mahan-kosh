047bafb04d1b6c8c5f41732622771c2a5c9ca02f
Gurpreet Singh <gur.singh2022@gmail.com>
2024-03-29 21:14:15 +0000

    chore: remove firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,17 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from './environments/environment';
 import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
-    importProvidersFrom(provideFirestore(() => getFirestore())),
-    provideStore()
-],
+  providers: [provideRouter(routes), provideAnimations(), provideStore()],
 };

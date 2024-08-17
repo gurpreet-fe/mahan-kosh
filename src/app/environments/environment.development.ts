48ebedef8ef8f03596184602bdeb29b672aab533
Gurpreet Singh <gur.singh2022@gmail.com>
2024-08-17 13:40:52 +0100

    chore: environment for dev and prod

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   meilisearch: {
     apiUrl: 'http://localhost:7700',
     apiKey: '4a08fa4ddc18643e82c78475e74226e25e4714caecbb08774a9047d35c725bfa',

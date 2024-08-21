41760e9b0c4dce10c9cfd39d83d008b3f90ae3c3
Gurpreet Singh <gur.singh2022@gmail.com>
2024-08-21 23:37:39 +0100

    chore: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,16 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { KeyboardComponent } from './components/keyboard/keyboard.component';
-import { SearchComponent } from './components/search/search.component';
-import { SharedModule } from './shared/shared.module';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'mk-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [
-    SharedModule,
-    RouterOutlet,
-    RouterLink,
-    HeaderComponent,
-    SearchComponent,
-    KeyboardComponent,
-  ],
+  imports: [RouterOutlet, LayoutComponent],
 })
 export class AppComponent {
   title = 'mahaan-kosh';
-  val = '';
-  list = signal<any>(undefined);
 
   constructor() {}
-
-  met(val: any) {
-    console.log(val);
-  }
 }

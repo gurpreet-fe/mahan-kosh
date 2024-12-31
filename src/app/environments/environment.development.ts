7cc72baad7dcc6adc1886e1cd256b757e8b58b89
Gurpreet Singh <gur.singh2022@gmail.com>
2024-12-31 18:35:23 +0000

    feat: meilisearch env index

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -3,5 +3,6 @@ export const environment = {
   meilisearch: {
     apiUrl: 'http://localhost:7700',
     apiKey: '4a08fa4ddc18643e82c78475e74226e25e4714caecbb08774a9047d35c725bfa',
+    mkIndex: 'mk-6'
   },
 };
--- src/app/services/meili-search.service.ts
+++ src/app/services/meili-search.service.ts
@@ -16,6 +16,8 @@ export class MeiliSearchService implements SearchServiceInterface {
   constructor() {}
 
   search(headword: string): Observable<SearchResponse> {
-    return from(this.client.index('mk').search(headword));
+    return from(
+      this.client.index(environment.meilisearch.mkIndex).search(headword)
+    );
   }
 }
